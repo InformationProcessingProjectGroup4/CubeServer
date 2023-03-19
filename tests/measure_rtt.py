443378a0574adb4ad3074f019d56acfe84cbd74e
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-03-19 11:56:22 +0000

    chore: update rtt measure script

--- tests/measure_rtt.py
+++ tests/measure_rtt.py
@@ -5,9 +5,23 @@ import requests
 # constatns
 URL_HOST = "ec2-35-177-122-51.eu-west-2.compute.amazonaws.com"
 URL_PORT = "5000"
-TEST_API = ["/api", "/api/progress"]
-TEST_DATA = [{}, { "username": "samuelpswang" }]
-COUNT = 20
+TEST_API = [
+    "/api", 
+    "/api/user/add", 
+    "/api/user", 
+    "/api/progress/update", 
+    "/api/progress", 
+    "/api/leaderboard"
+]
+TEST_DATA = [
+    {}, 
+    { "username": "rtt1", "password": "guest" },
+    { "username": "rtt1", "password": "guest" },
+    { "username": "rtt1", "score": [2000, 0, 0, 0, 0], "level": [2, 0, 0, 0, 0], "progress": {} },
+    { "username": "rtt1" },
+    [{ "level": 1, "count": 5}]
+]
+COUNT = 10
 
 # measure rtt
 start = time.time()
@@ -21,9 +35,9 @@ for i in range(len(TEST_API)):
             print(f"{response.status_code}: {json.loads(response.content.decode('utf-8'))}")
     end = time.time()
     rtt = ((end-start)/COUNT)*1000
-    print("---")
+    print(f"----- TEST #{i+1} -----")
     print(f"api: {TEST_API[i]}")
-    print(f"test data: {TEST_DATA[i]}")
-    print(f"request count: {COUNT}")
+    print(f"test_data: {TEST_DATA[i]}")
+    print(f"request_count: {COUNT} times")
     print(f"rtt: {rtt} ms")
-    print("---")
+    print(f"-----------------------")
